13f98f04fd56461cff43367ec525d75fcadf0501
Swapnil Nagothanekar <itsaswapnil@gmail.com>
2025-09-28 00:02:20 +0530

    thss rge

--- Hello.java
+++ Hello.java
@@ -5,9 +5,12 @@ class Hello{
     Log logger = LogFactory.getLog(Hello.class);
     public static void main(String[] args) {
         logger.info("This is an info message");
-
+        logger.info("This is an info dd message");
         logger.info("This is an info ff message");
         logger.info("This is an info gghj message");
-
     }
+    public static void test(){
+        System.out.println("This is a test method");
+    }
+
 }
