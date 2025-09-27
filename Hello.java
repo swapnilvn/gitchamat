f51a2416ddf25c6a550e973a230b3b48a0e712e0
SwapniL Nagothanekar <itsaswapnil@gmail.com>
2025-09-28 00:00:04 +0530

    Asar (#11)
    * series changes
    
    * thss rge
    
    * thss rge
    
    * thss rge

---
+++ Hello.java
@@ -0,0 +1,13 @@
+
+
+class Hello{
+
+    Log logger = LogFactory.getLog(Hello.class);
+    public static void main(String[] args) {
+        logger.info("This is an info message");
+
+        logger.info("This is an info ff message");
+        logger.info("This is an info gghj message");
+
+    }
+}
